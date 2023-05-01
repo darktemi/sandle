ffa38468c6701ff55cc2adbaaa252f946784c766
darktemi <darktemi90@nate.com>
2023-05-01 17:19:15 +0900

    jenkins test

--- sandle-app/src/main/resources/static/auth/login_form.js
+++ sandle-app/src/main/resources/static/auth/login_form.js
@@ -15,7 +15,7 @@ document.querySelector("#btn-login").onclick = () => {
   const form = document.querySelector("#login-form");
   const formData = new FormData(form);
 
-  fetch("../login", {
+  fetch("./login", {
     method: "post",
     body: formData,
   })
