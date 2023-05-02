5b2dbea4a86b27e6c5f13d6f3cc784557acc2166
darktemi <darktemi90@nate.com>
2023-05-02 15:12:38 +0900

    location test

--- sandle-app/src/main/java/bitcamp/myapp/service/impl/DefaultMountainInfoService.java
+++ sandle-app/src/main/java/bitcamp/myapp/service/impl/DefaultMountainInfoService.java
@@ -1,8 +1,10 @@
 package bitcamp.myapp.service.impl;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import bitcamp.myapp.dao.MountainInfoDao;
 import bitcamp.myapp.service.MountainInfoService;
 import bitcamp.myapp.vo.MountainInfo;
@@ -10,22 +12,22 @@ import bitcamp.myapp.vo.MountainInfo;
 @Service
 public class DefaultMountainInfoService implements MountainInfoService {
 
-  @Autowired private MountainInfoDao mountainInfoDao;
+	@Autowired private MountainInfoDao mountainInfoDao;
 
-  @Override
-  public List<MountainInfo> list(String keyword) {
-    return mountainInfoDao.findAll(keyword);
-  }
+	@Override
+	public List<MountainInfo> list(String keyword) {
+		return mountainInfoDao.findAll(keyword);
+	}
 
-  @Override
-  public List<MountainInfo> get(int regionId) {
-    return mountainInfoDao.findByRegionId(regionId);
-  }
+	@Override
+	public List<MountainInfo> get(int keyword) {
+		return mountainInfoDao.findByRegion(keyword);
+	}
 
-  @Override
-  public List<MountainInfo> get(int regionId, String title) {
-    return mountainInfoDao.findByRegionIdAndAddress(regionId, title);
-  }
+	@Override
+	public List<MountainInfo> get(int regionId, String title) {
+		return mountainInfoDao.findByRegionAndAddress(regionId, title);
+	}
 
 }
 
