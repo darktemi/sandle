c6cc9182988c600ae9c56ef18301ff890cc2e7bb
darktemi <darktemi90@nate.com>
2023-05-02 14:54:31 +0900

    location test

--- sandle-app/bin/main/static/auth/login_form.js
+++ sandle-app/bin/main/static/auth/login_form.js
@@ -26,7 +26,7 @@ document.querySelector("#btn-login").onclick = () => {
       if (result.status == "success") {
         alert("로그인 success!");
         console.log(result.status);
-        location.href = "../qna/list.html";
+        location.href = "../index.html";
       } else {
         alert("로그인 실패!");
         document.querySelector('input[name="email"]').value = "";
